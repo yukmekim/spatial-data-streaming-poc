ea5f7bde0257f17360eb56bd6a1c82e674bdbc9d
yukmekim <gyjj1243@gmail.com>
2026-03-11 17:25:16 +0900

    feat: unity 미사용으로 쿼터니언 값 작성 제외

--- src/main/java/dev/yukmekim/spatialdatastreamingpoc/dto/request/PoseDto.java
+++ src/main/java/dev/yukmekim/spatialdatastreamingpoc/dto/request/PoseDto.java
@@ -1,5 +1,6 @@
 package dev.yukmekim.spatialdatastreamingpoc.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +18,15 @@ public class PoseDto {
     @NotNull
     private Double tz;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Double qw;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Double qx;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Double qy;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Double qz;
 }
